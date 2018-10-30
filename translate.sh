cca49d8d70bb9b3470798fdf2ccba7d1b517fbae
123 <123@123>
2018-10-30 11:26:57 +0800

    ok

---
+++ translate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+q=`cat <<EOF 
+$1
+EOF`
+from=en
+to=zh
+appid=20181029000226645
+salt=123456
+key=kT1M8aSyZ32M1FAg5jka
+sign=`echo -n "$appid$q$salt$key" | md5sum`
+sign=`echo $sign|awk '{print $1}'`
+post="q=$q&from=$from&to=$to&appid=$appid&salt=$salt&sign=$sign"
+a=`curl -s http://api.fanyi.baidu.com/api/trans/vip/translate -X POST -d "$post"`
+a=${a#*dst\":\"}
+echo -e ${a%%\"*}
+#curl http://api.fanyi.baidu.com/api/trans/vip/translate -X POST -d "q=$q&from=$from&to=$to&appid=$appid&salt=$salt&sign=$sign"
